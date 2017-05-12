61c6be66c8efb4db7507331ccb3e8758ec94864e
shybyte <ein@volloeko.de>
2017-05-12 13:28:07 +0200

    Added sdl2 audio example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+extern crate sdl2;
+
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
+use std::time::Duration;
+
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        // Generate a square wave
+        for x in out.iter_mut() {
+            *x = match self.phase {
+                0.0...0.5 => self.volume,
+                _ => -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, synth!");
+    println!("Starting Rusty Synth...");
+
+
+    let sdl_context = sdl2::init().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1),  // mono
+        samples: None       // default sample size
+    };
+
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        // Show obtained AudioSpec
+        println!("{:?}", spec);
+
+        // initialize the audio callback
+        SquareWave {
+            phase_inc: 440.0 / spec.freq as f32,
+            phase: 0.0,
+            volume: 0.25
+        }
+    }).unwrap();
+
+    // Start playback
+    device.resume();
+
+    // Play for 2 seconds
+    std::thread::sleep(Duration::from_millis(2000));
+
 }
